5a10c0f8e139109bc09c6ff60025901f98f22012
Albano88 <claudioalbano.88@gmail.com>
2022-01-17 12:36:54 +0100

    ngModel Modifiche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
+
+//componente sostituisce dinamicamente il selettore app-root
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  name = 'Claudio';
+  title ='Welcome'
 }
